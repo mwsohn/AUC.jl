14f5916f78f7180963418083a3440c7e65e9caf6
paulstey <paulstey@gmail.com>
2017-08-18 13:55:36 -0400

    fixed renamed function

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using Base.Test
 y = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1]
 y_score = [0.3, 0.2, 0.3, 0.23, 0.5, 0.34, 0.45, 0.54, 0.6, 0.7, 0.8, 0.65, 0.5, 0.4, 0.3, 0.2, 0.6, 0.7, 0.5, 0.2, 0.1, 0.7, 0.2, 0.7, 0.4]
 
-res = roc_auc_score(y, y_score)
+res = auc(y, y_score)
 
 @test isequal(res, 0.7266666666666667)
 
 
 
-res2 = AUC.auc(y, y_score, reorder = true)
+res2 = AUC._auc(y, y_score, reorder = true)
 
 @test isequal(res2, 0.44999999999999996)
